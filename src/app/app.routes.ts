fe15a4d632d8da67675332eb55d8330cc241192a
GabrielCornejoB <gabrielcorbot@gmail.com>
2024-01-13 11:46:18 -0500

    Navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CountriesComponent } from './countries/countries.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: CountriesComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+  },
+];
